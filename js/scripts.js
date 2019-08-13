a7162b74b4d810b50864e1d2e8f1dfd92defb024
Stewart ilondanga <stilondanga@gmail.com>
2019-08-13 23:41:03 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -16,19 +16,20 @@ function drawing(elem) {
   }
   elem.style.backgroundColor = "rgb(" + colorray[0] + "," + colorray[1] + "," + colorray[2] + ")";
 }
-/*function reset(){
-	var all=document.getElementById('box').getElementsByTagName('input');
-	for(i=0;i<all.length;i++){
-		all[i].checked=false;
-		all[i].style.backgroundColor="transparent";
-	}
+
+function reset() {
+  var all = document.getElementById('box').getElementsByTagName('input');
+  for (i = 0; i < all.length; i++) {
+    all[i].checked = false;
+    all[i].style.backgroundColor = "transparent";
+  }
 }
 
 var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
