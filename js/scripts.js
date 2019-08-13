15cf3da03f49d880a70b0dce2cfd379767217b89
Stewart ilondanga <stilondanga@gmail.com>
2019-08-13 23:39:15 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,15 @@ function setRGB(rgb, value) {
   colorray[rgb] = value;
   color.style.backgroundColor = "rgb(" + colorray[0] + "," + colorray[1] + "," + colorray[2] + ")";
 }
-/*function drawing(elem){
-	if(!elem.checked){
-		elem.style.backgroundColor="transparent";
-		return;
-	}
-	elem.style.backgroundColor="rgb("+colorray[0]+","+colorray[1]+","+colorray[2]+")";
+
+function drawing(elem) {
+  if (!elem.checked) {
+    elem.style.backgroundColor = "transparent";
+    return;
+  }
+  elem.style.backgroundColor = "rgb(" + colorray[0] + "," + colorray[1] + "," + colorray[2] + ")";
 }
-function reset(){
+/*function reset(){
 	var all=document.getElementById('box').getElementsByTagName('input');
 	for(i=0;i<all.length;i++){
 		all[i].checked=false;
